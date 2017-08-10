47ca8b710ffbd4713add8560ab2998ee92b4b67b
Brian Dhang <briandhang@Brians-Mac.local>
2017-08-11 02:24:49 +0700

    1

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import {View, Text} from 'react-native';
 export default class App extends Component {
   render(){
     return(
-      <View>
-        <Text>Hello world</Text>
+      <View style={{paddingTop: 20}}>
+        <Text>Hello world from Brian Dhang</Text>
       </View>
     );
   }
